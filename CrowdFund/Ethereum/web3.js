32e9d60c50e64b5fc8e63fb08374e33d941442cd
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2024-06-04 14:50:08 +0530

    Update web3.js

--- CrowdFund/Ethereum/web3.js
+++ CrowdFund/Ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://sepolia.infura.io/v3/3238ca6f194a4f87a2f3e94f91ed43f4"
+    url
   );
   web3 = new Web3(provider);
 }
