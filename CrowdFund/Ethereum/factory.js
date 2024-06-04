9aac16a766b5560ecffc6c0f6185c16a63c2e73a
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2024-06-04 14:49:28 +0530

    Update factory.js

--- CrowdFund/Ethereum/factory.js
+++ CrowdFund/Ethereum/factory.js
@@ -4,7 +4,7 @@ import campaignFactory from './build/CampaignFactory.json'
 console.log(campaignFactory.abi)
 const instance = new web3.eth.Contract(
     campaignFactory.abi,
-  '0x2F638cb6ee4DE46Bf58B36b7F9a80a1128E1ECCa'
+      address
 );
 
 export default instance;
