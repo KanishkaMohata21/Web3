fbe3aeb8a48085d86f32091afca1485a03fbefd4
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2024-06-20 03:56:09 +0530

    Update Campaign.sol

--- CrowdFund/Ethereum/contracts/Campaign.sol
+++ CrowdFund/Ethereum/contracts/Campaign.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-//variables are storage and arguments or parameters are memory
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimumContribution) public {
-        address newCampaign = address(new Campaign(minimumContribution,msg.sender));
+    function createCampaign(
+        uint minimumContribution,
+        string memory name,
+        string memory description,
+        string memory outcome,
+        uint estimatedTime
+    ) public {
+        address newCampaign = address(new Campaign(
+            minimumContribution,
+            msg.sender,
+            name,
+            description,
+            outcome,
+            estimatedTime
+        ));
         deployedCampaigns.push(newCampaign);
     }
 
@@ -14,8 +26,6 @@ contract CampaignFactory {
         return deployedCampaigns;
     }
 }
-
-//if a contract is called from a contract that the contract is manager not the user
 
 contract Campaign {
     struct Request {
@@ -32,17 +42,34 @@ contract Campaign {
     mapping(address=>bool) public approvers;
     uint public approverCount;
 
+    string public projectName;
+    string public projectDescription;
+    string public projectOutcome;
+    uint public estimatedTime;
+
     modifier restricted() {
         require(msg.sender == manager, "This function is restricted to the manager");
         _;
     }
 
-    constructor(uint _minimumContribution, address _manager) {
+    constructor(
+        uint _minimumContribution,
+        address _manager,
+        string memory name,
+        string memory description,
+        string memory outcome,
+        uint _estimatedTime
+    ) {
         minimumContribution = _minimumContribution;
         manager = _manager;
+        projectName = name;
+        projectDescription = description;
+        projectOutcome = outcome;
+        estimatedTime = _estimatedTime;
     }
+
     function contribute() public payable {
-        require(msg.value>minimumContribution);
+        require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
         approverCount++;
     }
@@ -55,7 +82,7 @@ contract Campaign {
         newRequest.complete = false;
         newRequest.approvalcount = 0;
     }
-    
+
     function approveRequest(uint index) public {
         Request storage request = requests[index];
         require(approvers[msg.sender]);
@@ -63,6 +90,7 @@ contract Campaign {
         request.approvals[msg.sender] = true;
         request.approvalcount++;
     }
+
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
         require(!request.complete, "Request is already complete");
@@ -70,23 +98,39 @@ contract Campaign {
         payable(request.recipient).transfer(request.value);
         request.complete = true;
     }
+
     function getSummary() public view returns (
-      uint, uint, uint, uint, address
-      ) {
+        uint, uint, uint, uint, string memory, string memory, string memory, uint,address
+    ) {
         return (
-          minimumContribution,
-          address(this).balance,
-          requests.length,
-          approverCount,
-          manager
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approverCount,
+            projectName,
+            projectDescription,
+            projectOutcome,
+            estimatedTime,
+            manager
         );
     }
-    
+
     function getRequestsCount() public view returns (uint) {
         return requests.length;
     }
-
 }
+
+
+//looping through a big array coasts a lot of gas so avoid it
+//searching through an mapping is constant time search 
+
+//in mapping keys are not stored 
+//we can access value by passing a key in lookup process then a hashing function is 
+//called on that key and then a index is generated to get the value
+
+//in mapping values are not iterable
+//in mapping all values exists
+
 
 //looping through a big array coasts a lot of gas so avoid it
 //searching through an mapping is constant time search 
